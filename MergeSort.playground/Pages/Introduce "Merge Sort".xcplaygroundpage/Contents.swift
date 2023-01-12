3de8873b6a45564636e58342a50dfeb97191a5ed
Apiphoom Chuenchompoo <apiphoom23@gmail.com>
2023-01-12 13:47:57 +0700

    update : add Introduction page

---
+++ MergeSort.playground/Pages/Introduce "Merge Sort".xcplaygroundpage/Contents.swift
@@ -0,0 +1,6 @@
+/*:
+# What is Merge Sort??
+ ![MergeSort](Merge-Sort-Algorithm.png)
+ Merge Sort is a divide and conquer algorithm that sorts an array by dividing it into two halves, sorting each half, and then merging the sorted halves back together. It is a stable sorting algorithm with a time complexity of O(n log n) in the worst and average cases, and O(n) in the best case. The basic idea behind the merge sort algorithm is that it divides the input array into two halves, recursively sorts each half, and then merges the two sorted halves back together. This sorting technique is very efficient for large data sets, and is often used in situations where performance is critical.
+- - -
+*/
